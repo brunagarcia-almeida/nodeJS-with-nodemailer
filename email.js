390a7da5963b90b6bbf66f3d1f835f19499bff34
brunagarcia-almeida <73366397+brunagarcia-almeida@users.noreply.github.com>
2024-02-08 19:47:50 +0000

    comentando ferramentas utilizadas

--- email.js
+++ email.js
@@ -1,29 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const porta = 443;
 
-app.get('/', (req, res) => {
-  res.send('Enviando e-mail com o Nodemailer!');
+app.get("/", (req, res) => {
+  res.send("Enviando e-mail com o Nodemailer!");
 });
-app.get('/sendemail', async (req, res) => {
-  // nodemailer.createTransport({
-  //   host: "smtp.example.com",
-  //   port: 587,
-  //   secure: false, // upgrade later with STARTTLS
-  //   auth: {
-  //     user: "username",
-  //     pass: "password",
-  //   },
-  // });
+app.get("/sendemail", async (req, res) => {
+  // usando https://mailtrap.io
   var transport = nodemailer.createTransport({
     host: "sandbox.smtp.mailtrap.io",
     port: 2525,
     auth: {
       user: "c1d9f08304f4ea",
-      pass: "a3a51b5e86086c"
-    }
+      pass: "a3a51b5e86086c",
+    },
   });
+  // usando https://nodemailer.com/message/
   var message = {
     from: "noreplay@reprograme-se.com",
     to: "reprograme-se@reprograme-se.com",
@@ -31,20 +24,21 @@ app.get('/sendemail', async (req, res) => {
     text: "Olá Bruna, \n\n Você solicitou alteração de senha. \n\n Equipe Reprograme-se.",
     html: "<p>Olá Bruna, <br><br> Você solicitou alteração de senha. <br><br>Equipe Reprograme-se.</p>",
   };
-  transport.sendMail(message, function(err) {
-    if(err) {
+  // usando https://nodemailer.com/usage/
+  transport.sendMail(message, function (err) {
+    if (err) {
       return res.status(400).json({
         erro: true,
-        mensagem: 'Erro: E-mail não enviado!'
+        mensagem: "Erro: E-mail não enviado!",
       });
     } else {
       return res.json({
         erro: false,
-        mensagem: 'E-mail enviado com sucesso!'
+        mensagem: "E-mail enviado com sucesso!",
       });
     }
   });
 });
 app.listen(porta, () => {
-  console.log('Servidor rodando na porta: ', porta);
-})
+  console.log("Servidor rodando na porta: ", porta);
+});
